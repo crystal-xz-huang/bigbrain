031afa59f97b7d21476e7d9a7ccde17749171dcf
Crystal <z5215571@ad.unsw.edu.au>
2025-06-20 15:20:34 +1000

    chore(seed): add seed script for User model

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import { prisma } from "@/lib/prisma"
+
+async function main() {
+  console.time("Seeding complete 🌱");
+
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      name: "Alice",
+    },
+  });
+
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+    },
+  });
+
+  console.timeEnd("Seeding complete 🌱");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+  
